a37d0de63bf94e9885038551523f9e39cb7035dd
Madhusudhankandukuri123 <madhusudhan@softsuave.com>
2019-06-09 20:07:33 +0530

    Added new component for viewing details of the product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent  {
+  myEvent(event) {
+    window.alert('The product has been shared!');
+  
+  }
+}
 
 
 /*
